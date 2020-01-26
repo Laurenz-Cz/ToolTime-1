16ea70f0017642d60c93da1c73a6214858becce8
DominicFuchs <59792807+DominicFuchs@users.noreply.github.com>
2020-01-26 10:52:43 +0100

    Update 200125_FuD_CodeComplete.ino

--- 200125_FuD_CodeComplete.ino
+++ 200125_FuD_CodeComplete.ino
@@ -1,18 +1,18 @@
-int degree = 0; //gibt an, in welcher Stellung sich das Fahrzeug befindet - Start ist bei 0
+int degree = 0;         //gibt an, in welcher Stellung sich das Fahrzeug befindet - Start ist bei 0
 
 //--------------Motoren-----------------------------------
 
 //Motoren
-int in1 = 8;  //rechte Seite
-int in2 = 7;  //rechte Seite
-int in3 = 4;  //linke Seite
-int in4 = 2;  //linke Seite
+int in1 = 8;            //rechte Seite
+int in2 = 7;            //rechte Seite
+int in3 = 4;            //linke Seite
+int in4 = 2;            //linke Seite
 
 //Drehzahlregulierung
-int ENA = 3;  //Drehzahlregelung rechte Seite
-int ENB = 9;  //Drehzahlregelung linke Seite
-int SPEED = 150;  //Analoge Werte geben die Geschwindigkeit zum geradeaus Fahren vor (0-255)
-int SpeedDrehen = 150; //Analoge Werte geben die Geschwindigkeit für das Drehen des Fahrzeugs vor (0-255)
+int ENA = 3;            //Drehzahlregelung rechte Seite
+int ENB = 9;            //Drehzahlregelung linke Seite
+int SPEED = 150;        //Analoge Werte geben die Geschwindigkeit zum geradeaus Fahren vor (0-255)
+int SpeedDrehen = 150;  //Analoge Werte geben die Geschwindigkeit für das Drehen des Fahrzeugs vor (0-255)
 
 //--------------Ultraschallsensoren---------------------------
 
@@ -35,8 +35,8 @@ long dauer3 = 0;
 long leftSensor = 0;
 
 
-void setup()  {
-  
+void setup()  
+{
   //------------------MOTORSTEUERUNG--------------------------
   
   pinMode(in1, OUTPUT); //rechte Seite
@@ -44,26 +44,28 @@ void setup()  {
   pinMode(in3, OUTPUT); //linke Seite
   pinMode(in4, OUTPUT); //linke Seite
 
-//------------------Ultraschallsensoren-----------------------
+  //------------------Ultraschallsensoren-----------------------
 
   // Ultraschallsensor 1
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
 
   // Ultraschallsensor 2
+  //Serial.begin(9600);
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
 
   // Ultraschallsensor 3
+  //Serial.begin(9600);
   pinMode(trigPin3, OUTPUT);
   pinMode(echoPin3, INPUT);
 }
 
 
-void loop() {
-  
-  //---------------Ultraschallsensoren-----------------------
+void loop() 
+{
+    //---------------Ultraschallsensoren-----------------------
   
   //Ultraschallsensor vorne
   digitalWrite(trigPin1, LOW);
@@ -186,7 +188,7 @@ void loop() {
       }
       else if(degree == 90)
       {
-        degree = -90;
+        degree -= 90;
         //Fahrzeug bleibt stehen
         digitalWrite(in1, LOW);
         digitalWrite(in2, LOW);
@@ -194,13 +196,13 @@ void loop() {
         digitalWrite(in4, LOW);
         delay(2000);
    
-        //drehen nach rechts um 180 grad
+        //drehen nach links um 180 grad
         analogWrite(ENA, SpeedDrehen); //speed 0 - 255
         analogWrite(ENB, SpeedDrehen); //speed 0 - 255
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
         delay(300); //Doppelter Delay
       }
       else if(degree == -90)
@@ -223,21 +225,18 @@ void loop() {
         delay(150);
       }
     }
-  //else if(rightSensor <= 20 && leftSensor <= 20)
-    else if(rightSensor <= 20 && leftSensor > 20)
+    else if(rightSensor <= 20 && leftSensor <= 20)
     {
-      if(degree = 90)
+      if(degree == 90)
       {
-        degree = -90;
+        degree -= 90;
         //Fahrzeug bleibt stehen 
         digitalWrite(in1, LOW);
         digitalWrite(in2, LOW);
         digitalWrite(in3, LOW);
         digitalWrite(in4, LOW);
         delay(2000);
-
-//---------------FEHLER-----------------------------------------------
-   
+  
         //drehen nach rechts um 180 grad
         analogWrite(ENA, SpeedDrehen); //speed 0 - 255
         analogWrite(ENB, SpeedDrehen); //speed 0 - 255
@@ -248,7 +247,6 @@ void loop() {
         delay(300); //doppelt Delay
       }
 
-//-------------------------------------------------------------------      
       else if(degree = -90)
       {
         degree = 90; 
@@ -269,27 +267,27 @@ void loop() {
         delay(300); //Doppeltes Delay
       }
     }
-      else if(rightSensor > 20 && leftSensor > 20)
+    else if(rightSensor > 20 && leftSensor > 20)
+    {
+      if(degree == 90)
       {
-        if(degree == 90)
-        {
-          degree = 0;
-          //Fahrzeug bleibt stehen
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, LOW);
-          digitalWrite(in3, LOW);
-          digitalWrite(in4, LOW);
-          delay(2000);
+        degree = 0;
+        //Fahrzeug bleibt stehen
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, LOW);
+        delay(2000);
    
-          //drehen nach rechts um 90 grad
-          analogWrite(ENA, SpeedDrehen); //speed 0 - 255
-          analogWrite(ENB, SpeedDrehen); //speed 0 - 255
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, HIGH);
-          digitalWrite(in3, HIGH);
-          digitalWrite(in4, LOW);
-          delay(150);
-        }
+        //drehen nach rechts um 90 grad
+        analogWrite(ENA, SpeedDrehen); //speed 0 - 255
+        analogWrite(ENB, SpeedDrehen); //speed 0 - 255
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        delay(150);
+      }
       else if(degree == -90)
       {
         degree = 0;
@@ -318,6 +316,10 @@ void loop() {
       }
     }
   }
+  
+  //Folgender Abschnitt: Wenn der Abstand zu allen Sensoren größer als der definierte Wert ist, dann dreht das Fahrzeug wieder auf 0°
+  //und fährt anschließend wieder gerade aus. (Wenn es auf -90° steht --> Drehung nach links, bei +90° --> Drehung nach rechts)
+  
   else if(frontSensor > 20 && leftSensor > 20 && rightSensor > 20)
   {
     if(degree == 90)
@@ -338,6 +340,19 @@ void loop() {
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW);
       delay(150);
+      
+      //-------------TESTBLOCK----------------------
+      
+      // vorwärts fahren
+      analogWrite(ENA, SPEED);  //speed 0 - 255
+      analogWrite(ENB, SPEED);  //speed 0 - 255
+      digitalWrite(in1, LOW);   //linke Seite
+      digitalWrite(in2, HIGH);  //linke Seite
+      digitalWrite(in3, LOW);   //rechte Seite
+      digitalWrite(in4, HIGH);  //rechte Seite
+      
+      //--------------------------------------------
+      
     }
     else if(degree == -90)
     {
@@ -349,7 +364,7 @@ void loop() {
       digitalWrite(in4, LOW);
       delay(2000);
    
-    // drehen nach links um 90 grad
+      //drehen nach links um 90 grad
       analogWrite(ENA, SpeedDrehen); //speed 0 - 255
       analogWrite(ENB, SpeedDrehen); //speed 0 - 255
       digitalWrite(in1, HIGH);
@@ -357,16 +372,34 @@ void loop() {
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH);
       delay(150);
+      
+      //-------------TESTBLOCK----------------------
+      
+      // vorwärts fahren
+      analogWrite(ENA, SPEED);  //speed 0 - 255
+      analogWrite(ENB, SPEED);  //speed 0 - 255
+      digitalWrite(in1, LOW);   //linke Seite
+      digitalWrite(in2, HIGH);  //linke Seite
+      digitalWrite(in3, LOW);   //rechte Seite
+      digitalWrite(in4, HIGH);  //rechte Seite
+      
+      //--------------------------------------------
+     
     }
-  }
-  else {
-    // vorwärts fahren
-    analogWrite(ENA, SPEED);  //speed 0 - 255
-    analogWrite(ENB, SPEED);  //speed 0 - 255
-    digitalWrite(in1, LOW);   //linke Seite
-    digitalWrite(in2, HIGH);  //linke Seite
-    digitalWrite(in3, LOW);   //rechte Seite
-    digitalWrite(in4, HIGH);  //rechte Seite
+    else
+    {
+      //-------------TESTBLOCK----------------------
+      
+      // vorwärts fahren
+      analogWrite(ENA, SPEED);  //speed 0 - 255
+      analogWrite(ENB, SPEED);  //speed 0 - 255
+      digitalWrite(in1, LOW);   //linke Seite
+      digitalWrite(in2, HIGH);  //linke Seite
+      digitalWrite(in3, LOW);   //rechte Seite
+      digitalWrite(in4, HIGH);  //rechte Seite
+      
+      //--------------------------------------------
+    }
   }
 }
 
